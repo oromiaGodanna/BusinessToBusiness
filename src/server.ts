af39769dd8da2660214b5e90324ab4b613621e40
Tunga Tessema <tungat72@gmail.com>
2020-02-26 12:18:51 +0300

    initial setup

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import * as express from "express";
+const app = express();
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://localhost/b2b', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => { console.log('Connected to mongoDB...')})
+  .catch( err => console.error('Could not connect to mongoDB....'));
+
+app.get("/", (req, res) => {
+    res.send("Hello World!!!")
+})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+     console.log(`Server is running in http://localhost:${PORT}`)
+});
+
+
