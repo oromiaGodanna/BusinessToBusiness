d98fd60a7405319dcbb69537fbbb10b0f39442b5
Mercy <tamenemihret@gmail.com>
2020-04-10 22:53:25 +0300

    update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import * as express from "express";
 const app = express();
 const mongoose = require('mongoose');
-
+const bodyParser = require('body-parser');
+const task = require('./routes/task');
 
 mongoose.connect('mongodb://localhost/b2b', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => { console.log('Connected to mongoDB...')})
   .catch( err => console.error('Could not connect to mongoDB....'));
+
+  app.use(bodyParser.json());
+  app.use('/tasks', task);
+
 
 app.get("/", (req, res) => {
     res.send("Hello World!!!")
