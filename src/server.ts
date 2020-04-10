27fb8c7a034a330121ed59e4d9f4675dd0cd78f5
Mercy <tamenemihret@gmail.com>
2020-04-10 21:39:14 +0300

    updating server.ts

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const customer = require('./routes/customer');
 const task = require('./routes/task');
+const subscription = require('./routes/subscription');
 
 mongoose.connect('mongodb://localhost/b2b', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => { console.log('Connected to mongoDB...')})
@@ -11,9 +12,10 @@ mongoose.connect('mongodb://localhost/b2b', { useNewUrlParser: true, useUnifiedT
 
 
 app.use(bodyParser.json());
-//customer middlerware
+//middlerwares
 app.use('/customers', customer);
 app.use('/tasks', task);
+app.use('/subscription', subscription);
 
 app.get("/", (req, res) => {
     res.send("Hello World!!!")
