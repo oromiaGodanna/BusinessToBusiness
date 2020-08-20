128f9ba4298c2758dadc2cc8cdec2cdce062a0a8
oromiaGodanna <ori.gsero@gmail.com>
2020-08-20 20:48:07 +0300

    (feat) added add review functionality

---
+++ src/routes/review.ts
@@ -0,0 +1,39 @@
+export {};
+const {Review, validateReview} = require('../models/review');
+const express = require('express');
+const router = express.Router();
+
+function getOrderByID(){
+    return {
+        status: "complete"
+    }
+}
+
+
+router.post('/addReview', async (req, res) => {
+
+    //validate the request 
+    console.log(req.body)
+    const { error } = validateReview(req.body); 
+    if (error) return res.status(400).send(error.details[0].message);
+
+
+    let review;
+    //check if user have a completed order for this product
+    if( getOrderByID().status == "complete"){
+            review = new Review({ 
+                productId: req.body.productId,
+                orderId: req.body.orderId,
+                userId: req.body.userId,
+                review: req.body.review
+              });
+              review = await review.save();
+              
+    }
+
+    res.send(review);
+
+});
+
+
+module.exports = router;
