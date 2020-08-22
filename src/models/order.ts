6af3eaa0ec7ac094acf09b1b7eb9b99ffcf39723
oromiaGodanna <ori.gsero@gmail.com>
2020-08-22 18:33:07 +0300

    (fix) fixed data types on schema

--- src/models/order.ts
+++ src/models/order.ts
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 const Joi = require('joi');
 
 mongoose.set('useFindAndModify', false);
+
 let shippingAddress = new Object();
+const orderStatus = ['Waiting for confirmation', 'Order canceled', 'Order Declined', 'Waiting for initial payment', 'Waiting for shipment', 'Waiting for final payment', 'Waiting for delivery confirmation', 'Delivered', 'Completed'];
 
 const Order = mongoose.model('Order', new mongoose.Schema({
     
@@ -30,17 +32,18 @@ const Order = mongoose.model('Order', new mongoose.Schema({
     shippingAddress: {
         type: shippingAddress,
         required: true,
+        default: {}
     },
     status: {
         type: String,
-        required: true,
-        Enum: ['Waiting for confirmation', 'Order canceled', 'Order Declined', 'Waiting for initial payment', 'Waiting for shipment', 'Waiting for final payment', 'Waiting for delivery confirmation', 'Delivered', 'Completed']
+        enum: orderStatus,
+        default: "Waiting for confirmation"
     },
     paymentIds: {
         type: [ mongoose.Schema.Types.ObjectId ],
         required: true,
     }
-}))
+},  { minimize: false }))
 
 function validateOrder(order){
     const schema = Joi.object({
@@ -48,11 +51,10 @@ function validateOrder(order){
         sellerId: Joi.objectId().required(),
         productIds: Joi.array().items(Joi.objectId()).min(1).required(),
         amount: Joi.number().integer().min(1).required(),
-        price: Joi.number().positive().required(),
-        shippingAddress: Joi.required(),
-        status: Joi.string().required(),
+        price: Joi.number().positive().precision(2).strict().required(),
+        shippingAddress: Joi.object().allow(null).required(),
+        status: Joi.string().valid(...orderStatus),
         paymentIds: Joi.array().items(Joi.objectId()).allow(null).required()
-        
     });
 
     return schema.validate(order);
