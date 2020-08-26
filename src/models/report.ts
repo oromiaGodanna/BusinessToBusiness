9131eef8989a7d873b9a8ea38f8a947728979595
Mercy <tamenemihret@gmail.com>
2020-08-26 22:39:58 +0300

    report module

--- src/models/report.ts
+++ src/models/report.ts
@@ -1,6 +1,9 @@
+import { DH_NOT_SUITABLE_GENERATOR } from "constants";
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Joi = require('joi');
+const ObjectId = Schema.Types.ObjectId;
 
 const reportSchema = new Schema({
     numOfUsers: {
@@ -26,7 +29,7 @@ const reportSchema = new Schema({
 const Report = mongoose.model('Report', reportSchema);
 module.exports = Report;
 
-module.exports.valifateReport = function(report){
+module.exports.valifateReport = function (report) {
     const schema = {
         numOfUsers: Joi.number().integer(),
         numOfOrders: Joi.number().integer(),
@@ -36,5 +39,116 @@ module.exports.valifateReport = function(report){
         endDate: Joi.date()
     };
     return Joi.validate(report, schema);
+}
+
+const adminReportSchema = new Schema({
+    totalUsers: Number,
+    Buyers: Number,
+    Sellers: Number,
+    Both: Number,
+    verfiedUsers: Number,
+    newelyRegistredUsers: Number,
+    usersRegion: [{
+        region: String,
+        number: Number
+    }],
+    usersSubscription: [{
+        type: String,
+        number: Number
+    }],
+    orders: {
+        declined: Number,
+        canceled: Number,
+        inShipment: Number,
+        deliverd: Number,
+        //inProgress: Number,
+        completed: Number,
+    },
+    disputes: {
+        total: Number,
+        closed: Number,
+        open: Number,
+    },
+    moneyTransfered: Float32Array,
+});
+const sellerReportSchema = new Schema({
+    //orders: [ObjectId],
+    orders: {
+        declined: Number,
+        canceled: Number,
+        inShipment: Number,
+        deliverd: Number,
+        //inProgress: Number,
+        completed: Number,
+    },
+    products: [ObjectId],
+    subscription: ObjectId,
+    rating: Number,
+    subscribers: Number
+});
+
+module.exports.filterUsers= function (users,startDate, endDate) {
+    let Users= {
+        Buyers: (users.filter(user => user.userType == 'Buyer')).length,
+        Sellers: (users.filter(user => user.userType == 'Seller')).length,
+        Both:   (users.filter(user => user.userType == 'Both')).length,
+        verfied : (users.filter(user => user.verfied == true)).length,
+        newelyRegistred: users.filter(user => user.registredDate >= startDate && user.registredDate <= endDate),
+       
+    }
+    return Users;
+}
+
+module.exports.populateRegions = function (regions, users) {
+    let userRegions = [];
+    regions.forEach(region => {
+        const r = {
+            region: region.name,
+            number: users.filter(user => user.region == region.id),
+        }
+        userRegions.push(r);
+    });
+    return userRegions;
+}
+
+module.exports.populateSubscripions = function (subscriptions, users) {
+    let userSubscription = [];
+    subscriptions.forEach(subscription => {
+        const s = {
+            subscription: subscriptions.name,
+            number: users.filter(user => user.subscription == subscription.id),
+        }
+        userSubscription.push(s);
+    });
+    return userSubscription;
+}
+
+module.exports.filterOrders = function (orders) {
+    let userOrders = {
+        declined: orders.filter(order => order.filter == 'declined').length,
+        canceled: orders.filter(order => order.status == 'canceled').length,
+        shipment: orders.filter(order => order.staus == 'shipment').length,
+        delivered: orders.filter(order => order.status == 'deliverd').length,
+        completed: orders.filter(order => order.status == 'completed').length,
+       
+    }
+    return userOrders;
+}
+
+module.exports.filterDisputes = function (disputes) {
+    let userDisputes = {
+        total: disputes.length,
+        closed: disputes.filter(dispute => dispute.filter == 'close').length,
+        open: disputes.filter(dispute => dispute.status == 'open').length,
+    }
+    return userDisputes;
+}
+
+module.exports.addMoneyTransfered = function(payments) {
+    let amount = 0.0;
+    payments.forEach(payment => {
+        amount += amount + payment.amountPaid;
+        return amount;
+    });
 }
 
--- src/routes/report.ts
+++ src/routes/report.ts
@@ -1,11 +1,102 @@
 //const Report = require('../models/Report');
 const express = require('express');
 const router = express.Router();
+const {filterUsers, populateRegions, populateSubscripions, filterOrders, addMoneyTransfered, filterDisputes} = require('../models/report');
+const {Customer, Seller, Buyer, Both} = require('../models/customer');
+const Order = require('../models/order');
+const Region = require('../models/region');
+const Payment = require('../models/payment');
+const Subscriptions = require('../models/subscription');
+const Dispute = require('../models/Dispute');
+const Rating = require('../models/rating');
 
 //get admin report
 
-router.get('/admin', async(req, res)=>{
-    res.send('Admin Report');
+// router.get('/admin/:startDate/:endDate', async(req, res) => {
+
+//     try{
+//         const report = {
+//             numOfUsers: Customer.find().length,
+//             numOfOrders: Order.find().length,
+//             amountOfMoneyTransfered: addAmount(Payment.find()),
+//             userRegion: Region.find().sort({name: 1}).select({name: 1}),
+//             startDate: req.params.startDate,
+//             endDate:  req.params.endDate
+//         }
+//         res.json({
+//             status: 200,
+//             success: true,
+//             msg: "Admin report successfully provided",
+//             report: report
+//         });
+//     }catch(error){
+//         res.json({
+//             status: 500,
+//             success: false,
+//             msg: "Admin report not failed",
+//             error: error
+//         })
+//     }
+// });
+
+
+router.get('/admin/:startDate/:endDate', async(req, res)=>{
+    const startDate = req.params.startDate;
+    const endDate = req.params.endDate;
+
+    const users = Customer.find();
+    //const newlyRegistred = registred(users, req.params.startDate, req.params.endDate);
+    const regions = Region.find();
+    const subscriptions = Subscriptions.find();
+    const orders = Order.find();
+    const disputes = Dispute.find();
+    const payments = Payment.find();
+
+    const Users = filterUsers(users, startDate, endDate);
+
+    const report = {
+        totalUsers: users.length,
+        Buyers: Users.Buyers,
+        Sellers: Users.Sellers,
+        Both:  Users.Both,
+        verfiedUsers: Users.verfied,
+        newelyRegistredUsers: Users.newlyRegistred,
+        region: populateRegions(regions, users),
+        subscription: populateSubscripions(subscriptions, users),
+        Order: filterOrders(orders),
+        disputes: filterDisputes(disputes),
+        moneyTransfered: addMoneyTransfered(payments),
+    }
+    res.send(report);
+});
+
+router.get('/user/:id/:startDate/:endDate', async(req, res) => {
+    //find seller
+    const seller = Seller.findById(req.params.id);
+    if(!seller) res.status(404).send('No user found with this id');
+    try{
+        const report = {
+            orders: filterOrders(seller.orderIds),
+            products: seller.products,
+            subscription: seller.subscription,
+            rating: Rating.find({userId: req.params.id}).select({ratingValue: 1}),
+            subscribers: seller.subscribers.length
+        }
+        res.json({
+            status: 200,
+            success: true,
+            msg: 'report successfully provided',
+            report: report
+        });
+    }catch(error){
+        res.json({
+            status: 500,
+            success: false,
+            msg: 'report failed',
+            error: error.msg
+        });
+    }
+   
 });
 
 //get user report
